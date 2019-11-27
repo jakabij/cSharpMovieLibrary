95c687b1c0c449729161384ae0a223f921ae73c0
viktor-ujhazi <viktor.ujhazi@gmail.com>
2019-11-27 14:55:29 +0100

    commit

---
+++ Reports.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpMovieLibrary
+{
+    class Reports
+    {
+
+        static String[] GetMoviesByGenre(Movies movie)
+        {
+            Prop
+            foreach (var item in movie)
+            {
+
+            }
+
+
+            return null;
+        }
+
+    }
+}
