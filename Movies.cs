50a3d6f8726bd43eb01737324dbc2e950f44eb67
jakabij <jakabistvanj@gmail.com>
2019-11-28 13:29:37 +0100

    guut 2.0

--- Movies.cs
+++ Movies.cs
@@ -13,18 +13,6 @@ namespace cSharpMovieLibrary
         public string Stars { get; set; }
         public string Imdb { get; set; }
 
-        public List<String> GetProperties()
-        {
-            List<String> list = new List<String>();
-            list.Add(Title);
-            list.Add(Genre);
-            list.Add(Director);
-            list.Add(ReleaseYear);
-            list.Add(Stars);
-            list.Add(Imdb);
-            return list;
-        }
-
         public Movie(string title,string genre, string director, string releaseYear, string stars, string imdb)
         {
             Title = title;
